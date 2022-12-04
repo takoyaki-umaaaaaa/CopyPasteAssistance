b2638f61f8fc5f8f18b36c9cdf2cf50009af94c9
Takoyaki_umaaaaaa <takoyaki_umaaaaaa@hotmail.com>
2022-12-04 18:02:43 +0900

    Add files via upload

--- CompressReleaseFile.bat
+++ CompressReleaseFile.bat
@@ -1,7 +1,7 @@
 @echo off
 rem makecabがshift-jisしか日本語ファイル名を受け付けないため、切り替える
 chcp 932
-makecab /F ReleaseCabInfo.ddf
+makecab /F CompressReleaseInfo.ddf
 
 rem makecabが出力先directory指定を無視するため、自前で後処理する
 move disk1\* release
