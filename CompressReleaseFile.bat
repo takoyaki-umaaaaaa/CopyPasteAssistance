ea6704db8f4f89a1956c414df6067c7ece2c7517
Takoyaki_umaaaaaa <takoyaki_umaaaaaa@hotmail.com>
2022-12-04 23:04:15 +0900

    Cab形式は諦めてzip形式にする

--- CompressReleaseFile.bat
+++ CompressReleaseFile.bat
@@ -1,9 +1,3 @@
 @echo off
-rem makecabがshift-jisしか日本語ファイル名を受け付けないため、切り替える
-chcp 932
-dir
-makecab /F CompressReleaseInfo.ddf
-
-rem makecabが出力先directory指定を無視するため、自前で後処理する
-move disk1\* release
-rmdir disk1
+chcp 65001
+powershell Compress-Archive -Path .\*.hta, Readme.md -DestinationPath release\コピペ補助.zip -Force
